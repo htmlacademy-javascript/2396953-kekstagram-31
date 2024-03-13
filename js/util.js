0c49c668d9c1e4b2a6ff7fe85767b882500cc39c
Marina Shustikova <vorobjeva.89@gmail.com>
2024-03-13 10:34:02 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,14 @@
+export {getRandomNumber, getUniqueRandomNumber}
+
+function getRandomNumber(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getUniqueRandomNumber(number, usedNumbers) {
+  let randomNumber;
+  do {
+    randomNumber = number;
+  } while (usedNumbers.has(randomNumber));
+  usedNumbers.add(randomNumber);
+  return randomNumber;
+}
