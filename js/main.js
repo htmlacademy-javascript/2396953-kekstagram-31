fa98d52e18bf1cae5b70727fa9508e10ad5c8f27
Marina Shustikova <vorobjeva.89@gmail.com>
2024-03-13 10:45:16 +0300

    Модуляция

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import {getRandomNumber, getUniqueRandomNumber} from './util.js';
-import {nameUser, avatar, arrDescription} from './data.js';
-import {generateRandomComment} from './generate-random-comment.js';
 import {arrayPhoto} from './array-photo.js';
+
+console.log(arrayPhoto);
