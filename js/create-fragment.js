1e50d38bf97c1ea8d07d1f669f85cf9874710f74
Marina Shustikova <vorobjeva.89@gmail.com>
2024-03-30 20:06:16 +0300

    Открывается и закрывается module8-task1

--- js/create-fragment.js
+++ js/create-fragment.js
@@ -1,29 +1,21 @@
+import {createEl} from './create-el.js';
+
 function createFragment(arrayPhoto) {
-  const pools = document.querySelector('.pictures');
-
-  const template = document.querySelector('#picture').content.querySelector('.picture');
-
-  const fragment = document.createDocumentFragment();
+  const wrapper = document.querySelector('.pictures');
 
   arrayPhoto.forEach((data) => {
     const {id, url, description, likes, comments} = data;
 
-    const element = template.cloneNode(true);
+    const element = document.querySelector('#picture').content.querySelector('.picture').cloneNode(true);
 
     element.querySelector('.picture__img').id = id;
     element.querySelector('.picture__likes').textContent = likes;
     element.querySelector('.picture__img').src = url;
     element.querySelector('.picture__img').alt = description;
+    element.querySelector('.picture__comments').textContent = comments.length;
 
-    comments.forEach((item) => {
-      const {message} = item;
-      element.querySelector('.picture__comments').textContent = message;
-    });
-
-    fragment.appendChild(element);
+    createEl(element, wrapper);
   });
-
-  pools.appendChild(fragment);
 }
 
 export {createFragment};
--- js/create-popup.js
+++ js/create-popup.js
@@ -1,11 +1,24 @@
+import {createEl} from './create-el.js';
+import {getRandomNumber} from './util.js';
+
 function createPopup(arrayPhoto) {
   const popupWrapper = document.querySelector(".big-picture");
   const popupImg = popupWrapper.querySelector("img");
-  const popupLikesCount = popupWrapper.querySelector(".likes-count");
+
+  const popupSocialHeader = popupWrapper.querySelector(".social__header");
+  const socialHeaderPicture = popupSocialHeader.querySelector(".social__picture");
+  const socialHeaderLikesCount = popupSocialHeader.querySelector(".likes-count");
+
+  const popupSocialComments = popupWrapper.querySelector(".social__comments");
+  const popupSocialCaption = popupWrapper.querySelector(".social__caption");
+
 
   const popupButtons = document.querySelectorAll(".picture");
   const popupClose = document.querySelector(".big-picture__cancel");
   const body = document.querySelector("body");
+
+  const commentShownCount = popupWrapper.querySelector(".social__comment-shown-count");
+  const commentTotalCount = popupWrapper.querySelector(".social__comment-total-count");
 
   function openPopup () {
     popupWrapper.classList.remove('hidden');
@@ -15,22 +28,45 @@ function createPopup(arrayPhoto) {
   function closePopup () {
     popupWrapper.classList.add('hidden');
     body.classList.remove('modal-open');
+    popupSocialComments.innerHTML = "";
   }
 
   popupButtons.forEach((el) => {
-    el.addEventListener('click', function(event) {
-      openPopup();
-
-      const img = el.querySelector('.picture__img');
+    el.addEventListener('click', function() {
       const imgId = el.querySelector('.picture__img').id;
-      const comments = el.querySelectorAll('.picture__comments');
-      const likes = parseInt(el.querySelector('.picture__likes').textContent);
+      const imgAvatar = `img/avatar-${getRandomNumber(6, 1)}.svg`;
 
-      popupImg.src = img.src;
-      popupLikesCount.textContent = likes;
+      let arrEl = arrayPhoto.filter(obj => obj.id === Number(imgId));
+      const {url, description, likes, comments} = arrEl[0];
 
-      console.log(arrayPhoto);
+      commentTotalCount.textContent = comments.length;
 
+      5 > comments.length ? commentShownCount.textContent = comments.length : commentShownCount.textContent = 5;
+
+      console.log(comments.length);
+
+      socialHeaderLikesCount.textContent = likes;
+      socialHeaderPicture.src = imgAvatar;
+      popupSocialCaption.textContent = description;
+      popupImg.src = url;
+
+      const wrapperSocialComments = document.querySelector('.social__comments');
+
+      comments.forEach((data, index) => {
+        const {name, avatar, message} = data;
+
+        const element = document.querySelector('#comment').content.querySelector('.social__comment').cloneNode(true);
+
+        element.querySelector('.social__picture').src = avatar;
+        element.querySelector('.social__picture').alt = name;
+        element.querySelector('.social__text').textContent = message;
+
+        if (index > 4) {return}
+
+        createEl(element, wrapperSocialComments);
+      });
+      
+      openPopup();
     });
   });
 
