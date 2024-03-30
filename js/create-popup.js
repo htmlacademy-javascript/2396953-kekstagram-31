29d54cf58ef0f063b8f7150a7113f80dc36ebd8e
Marina Shustikova <vorobjeva.89@gmail.com>
2024-03-30 22:32:46 +0300

    8.15. Открывается и закрывается (часть 2)

--- js/create-popup.js
+++ js/create-popup.js
@@ -15,17 +15,21 @@ function createPopup(arrayPhoto) {
   const popupButtons = document.querySelectorAll('.picture');
   const popupClose = document.querySelector('.big-picture__cancel');
   const body = document.querySelector('body');
+  const commentloader = popupWrapper.querySelector('.comments-loader');
   const commentShownCount = popupWrapper.querySelector('.social__comment-shown-count');
   const commentTotalCount = popupWrapper.querySelector('.social__comment-total-count');
+
   function openPopup () {
     popupWrapper.classList.remove('hidden');
     body.classList.add('modal-open');
   }
+
   function closePopup () {
     popupWrapper.classList.add('hidden');
     body.classList.remove('modal-open');
     popupSocialComments.innerHTML = '';
   }
+
   popupButtons.forEach((el) => {
     el.onclick = () => {
       const imgId = el.querySelector('.picture__img').id;
@@ -33,33 +37,66 @@ function createPopup(arrayPhoto) {
       const arrEl = arrayPhoto.filter((obj) =>
         obj.id === Number(imgId)
       );
+
       const {url, description, likes, comments} = arrEl[0];
       commentTotalCount.textContent = comments.length;
-      if (5 > comments.length) {
+
+      if (5 >= comments.length) {
         commentShownCount.textContent = comments.length;
+        commentloader.classList.add('hidden');
       } else {
+        commentloader.classList.remove('hidden');
         commentShownCount.textContent = 5;
       }
+
       socialHeaderLikesCount.textContent = likes;
       socialHeaderPicture.src = imgAvatar;
       popupSocialCaption.textContent = description;
       popupImg.src = url;
       const wrapperSocialComments = document.querySelector('.social__comments');
+
       comments.forEach((data, index) => {
         const {name, avatar, message} = data;
         const element = document.querySelector('#comment').content.querySelector('.social__comment').cloneNode(true);
+
         element.querySelector('.social__picture').src = avatar;
         element.querySelector('.social__picture').alt = name;
         element.querySelector('.social__text').textContent = message;
-        if (index > 4) {
-          return;
-        }
+
         createEl(element, wrapperSocialComments);
       });
+
+      const elements = document.querySelectorAll('.social__comment');
+
+      elements.forEach((el, index) => {
+        if (index > 4) {
+          el.classList.add('hidden');
+        }
+      });
+
       openPopup();
     };
   });
+
   popupClose.onclick = () => closePopup();
+
+  commentloader.onclick = () => {
+    const socialCommentsHidden = document.querySelectorAll('.social__comment.hidden');
+
+    socialCommentsHidden.forEach((el, index) => {
+      if (index < 5) {
+        el.classList.remove('hidden');
+      }
+    });
+
+    const count = commentShownCount.textContent;
+    commentShownCount.textContent = Number(count) + 5;
+
+    if (Number(commentShownCount.textContent) > Number(commentTotalCount.textContent)) {
+      commentShownCount.textContent = Number(commentTotalCount.textContent);
+      commentloader.classList.add('hidden');
+    }
+  }
 }
 
 export {createPopup};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {arrayPhoto} from './array-photo.js';
+import {createFragment} from './create-fragment.js';
 import {createPopup} from './create-popup.js';
 
+createFragment(arrayPhoto);
 createPopup(arrayPhoto);
