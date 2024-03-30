05a85a19a544b05a12c504351ec92085d0cf9109
Marina Shustikova <vorobjeva.89@gmail.com>
2024-03-30 20:47:12 +0300

    8.14. Открывается и закрывается

--- js/create-popup.js
+++ js/create-popup.js
@@ -12,25 +12,20 @@ function createPopup(arrayPhoto) {
   const popupSocialComments = popupWrapper.querySelector('.social__comments');
   const popupSocialCaption = popupWrapper.querySelector('.social__caption');
 
-
   const popupButtons = document.querySelectorAll('.picture');
   const popupClose = document.querySelector('.big-picture__cancel');
   const body = document.querySelector('body');
-
   const commentShownCount = popupWrapper.querySelector('.social__comment-shown-count');
   const commentTotalCount = popupWrapper.querySelector('.social__comment-total-count');
-
   function openPopup () {
     popupWrapper.classList.remove('hidden');
     body.classList.add('modal-open');
   }
-
   function closePopup () {
     popupWrapper.classList.add('hidden');
     body.classList.remove('modal-open');
     popupSocialComments.innerHTML = '';
   }
-
   popupButtons.forEach((el) => {
     el.onclick = () => {
       const imgId = el.querySelector('.picture__img').id;
@@ -39,38 +34,28 @@ function createPopup(arrayPhoto) {
         obj.id === Number(imgId)
       );
       const {url, description, likes, comments} = arrEl[0];
-
       commentTotalCount.textContent = comments.length;
-
       if (5 > comments.length) {
         commentShownCount.textContent = comments.length;
       } else {
         commentShownCount.textContent = 5;
       }
-
       socialHeaderLikesCount.textContent = likes;
       socialHeaderPicture.src = imgAvatar;
       popupSocialCaption.textContent = description;
       popupImg.src = url;
-
       const wrapperSocialComments = document.querySelector('.social__comments');
-      
       comments.forEach((data, index) => {
         const {name, avatar, message} = data;
-
         const element = document.querySelector('#comment').content.querySelector('.social__comment').cloneNode(true);
-
         element.querySelector('.social__picture').src = avatar;
         element.querySelector('.social__picture').alt = name;
         element.querySelector('.social__text').textContent = message;
-
         if (index > 4) {
           return;
         }
-
         createEl(element, wrapperSocialComments);
       });
-
       openPopup();
     };
   });
