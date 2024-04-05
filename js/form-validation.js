ad9af413f59690e11142f19feea261461d91e3a5
Marina Shustikova <vorobjeva.89@gmail.com>
2024-04-05 20:36:29 +0300

    Исправления отправки при валидации

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,82 +1,77 @@
-import {stopEsc} from './util.js';
+let valid = true;
+const validationFields = document.querySelectorAll('.img-upload__text input');
+const descriptionInput = document.querySelector('.text__description');
+const descriptionMaxLength = descriptionInput.getAttribute('maxlength');
+const hashtagsInput = document.querySelector('.text__hashtags');
+const errorWrapper = document.querySelector('.img-upload__field-wrapper--error');
+const hashtagsMaxValue = 5;
+const hashtagMaxLength = 20;
 
-function validation () {
-  let valid = true;
-  const validationFields = document.querySelectorAll('.img-upload__text input');
-  const descriptionInput = document.querySelector('.text__description');
-  const descriptionMaxLength = descriptionInput.getAttribute('maxlength');
-  const hashtagsInput = document.querySelector('.text__hashtags');
-  const errorWrapper = document.querySelector('.img-upload__field-wrapper--error');
-  const hashtagsMaxValue = 5;
-  const hashtagMaxLength = 20;
-
-  validationFields.forEach(field = () => {
-    field.addEventListener('keydown', stopEsc);
+validationFields.forEach(field => {
+  field.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      event.stopPropagation();
+    }
   });
+});
 
-  hashtagsInput.addEventListener('input', () => {
-    const noEmptyStringRex = /\S/;
-    const hashtags = hashtagsInput.value.trim().toLowerCase().split(' ').filter(noEmptyStringRex.test.bind(noEmptyStringRex));
+hashtagsInput.addEventListener('input', () => {
+  valid = true;
+  const noEmptyStringRex = /\S/;
+  const hashtags = hashtagsInput.value.trim().toLowerCase().split(' ').filter(noEmptyStringRex.test.bind(noEmptyStringRex));
 
-    const uniqueHashtags = new Set(hashtags);
+  const uniqueHashtags = new Set(hashtags);
 
-    for (const hashtag of hashtags) {
-      if (hashtag.length === 0) {
-        continue;
-      }
-
-      if (!hashtag.startsWith('#')) {
-        displayError('Введён невалидный хэштег');
-        return;
-      }
-
-      if (hashtag.length === 1) {
-        displayError('Введён невалидный хэштег');
-        return;
-      }
-
-      if (hashtag.length > hashtagMaxLength) {
-        displayError('Введён невалидный хэштег');
-        return;
-      }
-
-      if (!/^[a-zA-Zа-яА-Я0-9]+$/.test(hashtag.slice(1))) {
-        displayError('Введён невалидный хэштег');
-        return;
-      }
-
-      if (uniqueHashtags.size !== hashtags.length) {
-        displayError('Хэштеги повторяются');
-        return;
-      }
-
-      if (hashtags.length > hashtagsMaxValue) {
-        displayError('Превышено количество хэштегов');
-        return;
-      }
+  for (const hashtag of hashtags) {
+    if (hashtag.length === 0) {
+      continue;
     }
 
-    errorWrapper.style.display = 'none';
-  });
-
-  descriptionInput.addEventListener('input', () => {
-    if (descriptionInput.length > descriptionMaxLength) {
-      displayError(`длина комментария больше ${descriptionMaxLength} символов`);
+    if (!hashtag.startsWith('#')) {
+      displayError('Введён невалидный хэштег');
       return;
     }
-  });
 
-  function displayError(errorMessage) {
-    valid = false;
-    errorWrapper.textContent = errorMessage;
-    errorWrapper.style.display = 'block';
+    if (hashtag.length === 1) {
+      displayError('Введён невалидный хэштег');
+      return;
+    }
+
+    if (hashtag.length > hashtagMaxLength) {
+      displayError('Введён невалидный хэштег');
+      return;
+    }
+
+    if (!/^[a-zA-Zа-яА-Я0-9]+$/.test(hashtag.slice(1))) {
+      displayError('Введён невалидный хэштег');
+      return;
+    }
+
+    if (uniqueHashtags.size !== hashtags.length) {
+      displayError('Хэштеги повторяются');
+      return;
+    }
+
+    if (hashtags.length > hashtagsMaxValue) {
+      displayError('Превышено количество хэштегов');
+      return;
+    }
   }
 
-  if (valid === false) {
-    document.addEventListener('#upload-submit').addEventListener('click', (event) => {
-      event.preventDefault();
-    });
+  errorWrapper.style.display = 'none';
+});
+
+descriptionInput.addEventListener('input', () => {
+  if (descriptionInput.length > descriptionMaxLength) {
+    displayError(`длина комментария больше ${descriptionMaxLength} символов`);
+    return;
   }
+});
+
+const displayError = (errorMessage) => {
+  valid = false;
+  errorWrapper.textContent = errorMessage;
+  errorWrapper.style.display = 'block';
 }
 
-export {validation};
+export {valid};
--- js/main.js
+++ js/main.js
@@ -2,34 +2,14 @@ import {arrayPhoto} from './array-photo.js';
 import {createFragment} from './create-fragment.js';
 import {createPopup} from './create-popup.js';
 import {uploadingPhotos} from './uploading-photos.js';
-import {validation} from './form-validation.js';
+import {valid} from './form-validation.js';
 
 createFragment(arrayPhoto);
 createPopup(arrayPhoto);
 
 uploadingPhotos();
-validation();
 
 
-// const uploadForm = document.querySelector('.img-upload__form');
-// const hashtagElement = document.querySelector('[name="hashtags"]');
-// const descriptionElement = document.querySelector('[name="description"]');
-
-//   const pristine = new Pristine(uploadForm, {
-//     // classTo: 'img-upload__field-wrapper',
-//     // errorTextParent: 'img-upload__field-wrapper',
-//     // errorTextClass: 'img-upload__field-wrapper--error',
-//   });
-
-// uploadForm.addEventListener('submit', (evt) => {
-//   evt.preventDefault();
-
-//   const isValid = pristine.validate();
-//   if (isValid) {
-//     console.log('Можно отправлять');
-//   } else {
-//     console.log('Форма невалидна');
-//   }
-// });
-
-
+document.querySelector('#upload-submit').addEventListener('click', (event) => {
+  valid ? document.querySelector('#upload-select-image').submit() : event.preventDefault();
+});
